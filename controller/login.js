3a303cb305d70f8c05f5bbaf371927f617cfcde1
EstebanSansart <julianestebansanabria@gmail.com>
2023-09-28 10:13:29 -0500

    feat: :tada: LoginPageCreated
    HTML and Styles done

---
+++ controller/login.js
@@ -0,0 +1,10 @@
+const loginForm = document.getElementById('login-form');
+const responseMessage = document.getElementById('response-message');
+
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById('login-username').value;
+    const password = document.getElementById('login-password').value;
+
+    
+});
---
+++ controller/register.js
@@ -0,0 +1,11 @@
+// register.js
+const registerForm = document.getElementById('register-form');
+const responseMessage = document.getElementById('response-message');
+
+registerForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById('register-username').value;
+    const password = document.getElementById('register-password').value;
+
+ 
+});
